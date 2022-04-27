563370c83b6191d71e84916079fff8786206e4a0
EPINOT <enzopinot44190@gmail.com>
2022-04-27 15:45:22 +0200

    grid update

--- game.js
+++ game.js
@@ -1,27 +1,8 @@
-var readline = require("readline-sync"); //lecture saisie du clavier
+import {grid, tab} from './grid.js';
 
-var ligne1=[0,0,0] 
-var ligne2=[0,0,0] 
-var ligne3=[0,0,0] 
-    
-var tab=[ligne1,ligne2,ligne3];
-
-function grid(tab)
+function game()
 {
-    for(var i=0; i < tab.length; i++) //accès aux lignes 1, 2, 3
-    {
-        var design ="";
-    
-        for(var j=0; j < tab[i].length; j++) // | | --> à chaque case (0)
-        {
-
-            design +="| |";
-
-        }
-
-        console.log(design);
-
-    }
+    grid(tab);
 }
 
-console.log(grid(tab));
+game();
